9ff12ada525c969e511ac5498d7d0446823bdf89
Ishtiak Ahmed <iahmed1606@gmail.com>
2021-01-27 23:25:08 +0600

    Gringotts account ready

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-console.log('app started');
+let AccountStatus = {
+    deposit: 0,
+    withdraw: 0,
+    balance: 0
+}
+
+
+// console.log('app started');
 const loginArea = document.querySelector('.login-area')
 const transArea = document.querySelector('#transaction-area')
 function loggedIn() {
@@ -15,3 +22,32 @@ window.addEventListener('keyup', function (event) {
         loggedIn();
     }
 });
+// Deposit Function
+let depositUI = document.querySelector('.deposit h4');
+let balanceUI = document.querySelector('.balance h4');
+let withdrawUI = document.querySelector('.withdraw h4');
+
+
+const depositBtn = document.getElementById('depositBtn');
+depositBtn.addEventListener('click', function () {
+    const depositAmount = parseFloat(document.getElementById("depositAmount").value);
+    AccountStatus.deposit += depositAmount;
+    AccountStatus.balance += depositAmount;
+    // console.log(AccountStatus.deposit);
+    // Update Deposit
+    depositUI.innerHTML = `$ ${AccountStatus.deposit}`;
+    // Update balance
+    balanceUI.innerHTML = `$ ${AccountStatus.balance}`;
+})
+// Withdraw Function
+const withdrawBtn = document.getElementById('withdrawBtn');
+withdrawBtn.addEventListener('click', function () {
+    const withdrawAmount = parseFloat(document.getElementById("withdrawAmount").value);
+    AccountStatus.withdraw += withdrawAmount;
+    AccountStatus.balance -= withdrawAmount;
+    // console.log(AccountStatus.deposit);
+    // Update Deposit
+    withdrawUI.innerHTML = `$ ${AccountStatus.withdraw}`;
+    // Update balance
+    balanceUI.innerHTML = `$ ${AccountStatus.balance}`;
+})
